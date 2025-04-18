e9b10bc7161cbf57b7e0b854000177b6ee3fe85e
Ian Winters <45905144+juliuseizure@users.noreply.github.com>
2025-04-18 13:16:21 -0500

    Modified for use
    Added my model(s) and label(s) for Yolov5, 8, and 11.  I saw no obvious performance difference between them.

--- android_app/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++ android_app/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -1,6 +1,13 @@
 package com.surendramaran.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "model.tflite"
-    const val LABELS_PATH = "labels.txt"
+//    const val MODEL_PATH = "yolov5_merged.tflite"
+//    const val MODEL_PATH = "yolov8_merged.tflite"
+//    const val MODEL_PATH = "yolov11_merged.tflite"
+//    const val LABELS_PATH = "merged_labels.txt"
+
+//    const val MODEL_PATH = "yolov5_multiclass.tflite"
+//    const val MODEL_PATH = "yolov8_multiclass.tflite"
+    const val MODEL_PATH = "yolov11_multiclass.tflite"
+    const val LABELS_PATH = "new_labels.txt"
 }
